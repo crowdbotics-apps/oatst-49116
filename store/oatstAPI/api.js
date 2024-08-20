6f2abd182cde745db4493aba26d2f0e01fe9b454
Crowdbotics <engineering@crowdbotics.com>
2024-08-20 22:09:44 +0000

    Update store.

--- store/oatstAPI/api.js
+++ store/oatstAPI/api.js
@@ -1,76 +1,94 @@
-import axios from "axios";
+import axios from "axios"
 const oatstAPI = axios.create({
   baseURL: "https://oatst-49116.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return oatstAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return oatstAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return oatstAPI.post(`/api/v1/login/`, payload);
+  return oatstAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return oatstAPI.post(`/api/v1/signup/`, payload);
+  return oatstAPI.post(`/api/v1/signup/`, payload)
 }
-
+function modules_openai_audio_transcription_create(payload) {
+  return oatstAPI.post(`/modules/openai/audio/transcription/`, payload)
+}
+function modules_openai_audio_translation_create(payload) {
+  return oatstAPI.post(`/modules/openai/audio/translation/`, payload)
+}
+function modules_openai_chat_completions_create(payload) {
+  return oatstAPI.post(`/modules/openai/chat/completions/`, payload)
+}
+function modules_openai_completion_create(payload) {
+  return oatstAPI.post(`/modules/openai/completion/`, payload)
+}
+function modules_openai_edits_create(payload) {
+  return oatstAPI.post(`/modules/openai/edits/`, payload)
+}
+function modules_openai_engines_retrieve(payload) {
+  return oatstAPI.get(`/modules/openai/engines/`)
+}
+function modules_openai_engines_retrieve_2(payload) {
+  return oatstAPI.get(`/modules/openai/engines/${payload.engine_id}/`)
+}
+function modules_openai_images_generations_create(payload) {
+  return oatstAPI.post(`/modules/openai/images/generations/`, payload)
+}
+function modules_openai_models_retrieve(payload) {
+  return oatstAPI.get(`/modules/openai/models/`)
+}
+function modules_openai_models_retrieve_2(payload) {
+  return oatstAPI.get(`/modules/openai/models/${payload.model_id}/`)
+}
 function rest_auth_login_create(payload) {
-  return oatstAPI.post(`/rest-auth/login/`, payload);
+  return oatstAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return oatstAPI.post(`/rest-auth/logout/`);
+  return oatstAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return oatstAPI.post(`/rest-auth/password/change/`, payload);
+  return oatstAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return oatstAPI.post(`/rest-auth/password/reset/`, payload);
+  return oatstAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return oatstAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return oatstAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return oatstAPI.post(`/rest-auth/registration/`, payload);
+  return oatstAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return oatstAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return oatstAPI.post(`/rest-auth/registration/resend-email/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return oatstAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return oatstAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return oatstAPI.get(`/rest-auth/user/`);
+  return oatstAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return oatstAPI.put(`/rest-auth/user/`, payload);
+  return oatstAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return oatstAPI.patch(`/rest-auth/user/`, payload);
+  return oatstAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_openai_audio_transcription_create,
+  modules_openai_audio_translation_create,
+  modules_openai_chat_completions_create,
+  modules_openai_completion_create,
+  modules_openai_edits_create,
+  modules_openai_engines_retrieve,
+  modules_openai_engines_retrieve_2,
+  modules_openai_images_generations_create,
+  modules_openai_models_retrieve,
+  modules_openai_models_retrieve_2,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
@@ -82,4 +100,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
--- store/oatstAPI/createCompletions.slice.js
+++ store/oatstAPI/createCompletions.slice.js
@@ -1,31 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const api_v1_login_create = createAsyncThunk(
-  "authTokens/api_v1_login_create",
+export const modules_openai_completion_create = createAsyncThunk(
+  "createCompletions/modules_openai_completion_create",
   async payload => {
-    const response = await apiService.api_v1_login_create(payload)
+    const response = await apiService.modules_openai_completion_create(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const authTokensSlice = createSlice({
-  name: "authTokens",
+const createCompletionsSlice = createSlice({
+  name: "createCompletions",
   initialState,
   reducers: {},
   extraReducers: builder => {
     builder
-      .addCase(api_v1_login_create.pending, (state, action) => {
+      .addCase(modules_openai_completion_create.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_login_create.fulfilled, (state, action) => {
+      .addCase(modules_openai_completion_create.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities.push(action.payload)
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_login_create.rejected, (state, action) => {
+      .addCase(modules_openai_completion_create.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
@@ -33,4 +33,7 @@ const authTokensSlice = createSlice({
       })
   }
 })
-export default { api_v1_login_create, slice: authTokensSlice }
+export default {
+  modules_openai_completion_create,
+  slice: createCompletionsSlice
+}
