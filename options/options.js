e32f7d883383aaf39ffb6ffd1fc5a6e159778c4c
Vadym Kolpak <vadym@tuluko.eu>
2024-08-21 00:55:23 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "oatst-49116",
-  url: "https://oatst-49116.botics.co",
-  api: "https://oatst-49116.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "oatst-49116",
+  "url": "https://oatst-49116.botics.co",
+  "api": "https://oatst-49116.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "add-review"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
